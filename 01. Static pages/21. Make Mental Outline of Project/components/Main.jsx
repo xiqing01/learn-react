2cfba4b3aafc92c0e2e4f5641bd46a799b65dd88
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-08 23:23:20 +0800

    Update Main.jsx

--- 01. Static pages/21. Make Mental Outline of Project/components/Main.jsx
+++ 01. Static pages/21. Make Mental Outline of Project/components/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main() {
   return (
-    <h1>Main compoent here<h1/>
+    <main>
+      <h1>Fun facts about React<h1/>
+      <ul>
+        <li>Was first released in 2013<li/>  
+        <li>Was originally created by Jordan Walke<li/>  
+        <li>Has well over 200K stars on GitHub<li/>  
+        <li>Is maintained by Meta<li/>  
+        <li>Powers thousands of enterPrise apps, including mobile apps<li/>  
+      <ul/>
+    <main/>
   )
 }
