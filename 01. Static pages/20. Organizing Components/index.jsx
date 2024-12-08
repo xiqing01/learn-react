21f4a0cf887c94aba57acab593171aa48fad1158
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-08 22:20:59 +0800

    Update index.jsx

--- 01. Static pages/20. Organizing Components/index.jsx
+++ 01. Static pages/20. Organizing Components/index.jsx
@@ -7,25 +7,9 @@ const root = createRoot(document.getElementById("root"))
  * separate files.
  */
 
-function MainContent() {
-    return (
-        <main>
-            <h1>Reason I am excited to learn React</h1>
-            <ol>
-                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                <li>I am more likely to get a job as a front end developer if I know React</li>
-            </ol>
-        </main>
-    )
-}
 
-function Footer() {
-    return (
-        <footer>
-            <small>© 2024 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
+
+
 
 function Page() {
     return (
