db517c04de219c0f1c06306cb0447970c71838b4
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-08 22:26:13 +0800

    Update index.jsx

--- 01. Static pages/20. Organizing Components/index.jsx
+++ 01. Static pages/20. Organizing Components/index.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client"
 import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 const root = createRoot(document.getElementById("root"))
 
 /**
