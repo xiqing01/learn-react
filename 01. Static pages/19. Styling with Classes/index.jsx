b6a171f31801096b18fc0380bd33de232031a3a4
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-08 21:59:20 +0800

    Update index.jsx

--- 01. Static pages/19. Styling with Classes/index.jsx
+++ 01. Static pages/19. Styling with Classes/index.jsx
@@ -12,7 +12,13 @@ function Header() {
     return (
         <header>
             <img src="react-logo.png" width="40px" alt="React logo" />
-            
+            <nav>
+                <ul className="nav-list">
+                    <li>Pricing<li/>
+                    <li>About<li/>
+                    <li>Contact<li/>
+                <ul/>    
+            <nav/>
         </header>
     )
 }
