6a69f8713e60d94934fa9f22b7fbf5342696aad7
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-08 23:09:50 +0800

    Update Navbar.jsx

--- 01. Static pages/21. Make Mental Outline of Project/components/Navbar.jsx
+++ 01. Static pages/21. Make Mental Outline of Project/components/Navbar.jsx
@@ -1,5 +1,11 @@
+
 export default function Navbar() {
   return (
-    <h1>Navbar component here<h1/>
+    <header>
+      <nav>
+        <img src="../react-logo.png" alt="React logo" />
+        <span>ReactFacts<span/>
+      <nav/>  
+    <header/>
   )
 }
