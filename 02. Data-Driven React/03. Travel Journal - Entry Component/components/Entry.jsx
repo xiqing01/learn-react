e4eb581011b98541a382f5fbe29d9e2ad792dc7b
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-10 17:17:27 +0800

    Update Entry.jsx

--- 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
+++ 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
@@ -21,7 +21,7 @@ export default function Entry() {
         <div>
           <img src="../images/marker.png" alt="map marker icon"/>
           <span>Japan</span>
-          <a href=""></a>
+          <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
         <div/>
     <article/>
   )
