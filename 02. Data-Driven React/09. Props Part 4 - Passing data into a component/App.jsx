2244617c3da0324f9e1d704a6438190b9f180d29
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-17 09:41:16 +0800

    Update App.jsx

--- 02. Data-Driven React/09. Props Part 4 - Passing data into a component/App.jsx
+++ 02. Data-Driven React/09. Props Part 4 - Passing data into a component/App.jsx
@@ -11,10 +11,27 @@ function App() {
             <Contact
                 img="./images/mr-whiskerson.png"
                 name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
             />
-            <Contact />
-            <Contact />
-            <Contact />
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Photo of Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact 
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
         </div>
     )
 }
@@ -24,91 +41,29 @@ export default App
 
 /**
  * 
-<article className="contact-card">
-    <img 
-        src="./images/mr-whiskerson.png"
-        alt="Photo of Mr. Whiskerson"
-    />
-    <h3>Mr. Whiskerson</h3>
-    <div className="info-group">
-        <img 
-            src="./images/phone-icon.png" 
-            alt="phone icon" 
-        />
-        <p>(212) 555-1234</p>
-    </div>
-    <div className="info-group">
-        <img 
-            src="./images/mail-icon.png" 
-            alt="mail icon"
-        />
-        <p>mr.whiskaz@catnap.meow</p>
-    </div>
-</article>
+
+
+
 
 <article className="contact-card">
     <img 
-        src="./images/fluffykins.png"
-        alt="Photo of Fluffykins"
-    />
-    <h3>Fluffykins</h3>
-    <div className="info-group">
-        <img 
-            src="./images/phone-icon.png" 
-            alt="phone icon" 
-        />
-        <p>(212) 555-2345</p>
-    </div>
-    <div className="info-group">
-        <img 
-            src="./images/mail-icon.png" 
-            alt="mail icon"
-        />
-        <p>fluff@me.com</p>
-    </div>
-</article>
-
-<article className="contact-card">
-    <img 
-        src="./images/felix.png"
-        alt="Photo of Felix"
-    />
-    <h3>Felix</h3>
-    <div className="info-group">
-        <img 
-            src="./images/phone-icon.png" 
-            alt="phone icon" 
-        />
-        <p>(212) 555-4567</p>
-    </div>
-    <div className="info-group">
-        <img 
-            src="./images/mail-icon.png" 
-            alt="mail icon"
-        />
-        <p>thecat@hotmail.com</p>
-    </div>
-</article>
-
-<article className="contact-card">
-    <img 
-        src="./images/pumpkin.png"
+        src=""
         alt="Photo of Pumpkin"
     />
-    <h3>Pumpkin</h3>
+    <h3></h3>
     <div className="info-group">
         <img 
             src="./images/phone-icon.png" 
             alt="phone icon" 
         />
-        <p>(0800) CAT KING</p>
+        <p></p>
     </div>
     <div className="info-group">
         <img 
             src="./images/mail-icon.png" 
             alt="mail icon"
         />
-        <p>pumpkin@scrimba.com</p>
+        <p></p>
     </div>
 </article>
  */
