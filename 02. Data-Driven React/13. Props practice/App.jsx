9b8c770932e2f87cba1a82c1112b278ba761333a
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-18 13:11:03 +0800

    Update App.jsx

--- 02. Data-Driven React/13. Props practice/App.jsx
+++ 02. Data-Driven React/13. Props practice/App.jsx
@@ -1,24 +1,12 @@
-/**
- * Challenge: create a page that displays your favorite jokes
- * - Create a Joke component in its own file.
- * - Import and render 4-5 <Joke /> components
- * - Each Joke should receive a "setup" prop and a "punchline" prop
- *   and render those however you'd like
- * - Use your favorite 2-part jokes (setup & punchline), or check
- *   jokes.md file for some examples.
- * 
- * EXTRA CREDIT:
- * Some jokes are only a punchline with no setup:
- * 
- * E.g.: "It’s hard to explain puns to kleptomaniacs because 
- * they always take things literally."
- * 
- * If you don't pass in a "question" prop, how might you make it only 
- * show the punchline?
- */
-
+import Joke from "./Joke"
 export default function App() {
     return (
-        <></>
+        <main>
+            <Joke />
+            <Joke />
+            <Joke />
+            <Joke />
+            <Joke />
+        </main>
     )
 }
