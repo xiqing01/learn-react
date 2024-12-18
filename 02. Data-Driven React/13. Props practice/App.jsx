c360f7d1d564234ae4c28da4057159b80cb694a6
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-18 13:17:14 +0800

    Update App.jsx

--- 02. Data-Driven React/13. Props practice/App.jsx
+++ 02. Data-Driven React/13. Props practice/App.jsx
@@ -2,11 +2,11 @@ import Joke from "./Joke"
 export default function App() {
     return (
         <main>
-            <Joke setup="" />
-            <Joke setup="" />
-            <Joke setup="" />
-            <Joke setup="" />
-            <Joke setup="" />
+            <Joke setup="I got my daughter a fridge for her birthday. Punchline: I can't wait to see her face light up when she opens it." />
+            <Joke setup="How did the hacker escape the police? Punchline: He just ransomware!" />
+            <Joke setup="Why don't pirates travel on mountain roads? Punchline: Scurvy." />
+            <Joke setup="Why do bees stay in the hive in the winter? Punchline: Swarm." />
+            <Joke setup=" What's the best thing about Switzerland? Punchline: I don't know, but the flag is a big plus!" />
         </main>
     )
 }
