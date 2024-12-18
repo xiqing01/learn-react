7e99702a9b620378c793c4e390b13103f00758df
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-18 13:14:24 +0800

    Update Joke.jsx

--- 02. Data-Driven React/13. Props practice/Joke.jsx
+++ 02. Data-Driven React/13. Props practice/Joke.jsx
@@ -1,5 +1,6 @@
 export default function Joke() {
   return (
-    <h1></h1>
+    <h1>I got my daughter a fridge for her birthday. Punchline: I can't wait to see her face light up when she opens it.
+</h1>
   )
 }
