8d1c3fc80519182729e2c4ea08034a1384095103
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-18 12:56:15 +0800

    Update Contact.jsx

--- 02. Data-Driven React/09. Props Part 4 - Passing data into a component/Contact.jsx
+++ 02. Data-Driven React/09. Props Part 4 - Passing data into a component/Contact.jsx
@@ -1,24 +1,24 @@
-export default function Contact() {
+export default function Contact(props) {
     return (
         <article className="contact-card">
             <img
-                src="./images/mr-whiskerson.png"
+                src={props.img}
                 alt="Photo of Mr. Whiskerson"
             />
-            <h3>Mr. Whiskerson</h3>
+            <h3>{props.name}</h3>
             <div className="info-group">
                 <img
                     src="./images/phone-icon.png"
                     alt="phone icon"
                 />
-                <p>(212) 555-1234</p>
+                <p>{props.phone}</p>
             </div>
             <div className="info-group">
                 <img
                     src="./images/mail-icon.png"
                     alt="mail icon"
                 />
-                <p>mr.whiskaz@catnap.meow</p>
+                <p>{props.email}</p>
             </div>
         </article>
     )
