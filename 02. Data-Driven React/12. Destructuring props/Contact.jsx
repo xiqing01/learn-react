0a27ec17c8f464fdea18e1db2daa699ad977cdd0
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-18 13:05:45 +0800

    Update Contact.jsx

--- 02. Data-Driven React/12. Destructuring props/Contact.jsx
+++ 02. Data-Driven React/12. Destructuring props/Contact.jsx
@@ -6,23 +6,23 @@ export default function Contact({img, name, phone, email}) {
     return (
         <article className="contact-card">
             <img
-                src={props.img}
+                src={img}
                 alt="Photo of Mr. Whiskerson"
             />
-            <h3>{props.name}</h3>
+            <h3>{name}</h3>
             <div className="info-group">
                 <img
                     src="./images/phone-icon.png"
                     alt="phone icon"
                 />
-                <p>{props.phone}</p>
+                <p>{phone}</p>
             </div>
             <div className="info-group">
                 <img
                     src="./images/mail-icon.png"
                     alt="mail icon"
                 />
-                <p>{props.email}</p>
+                <p>{email}</p>
             </div>
         </article>
     )
