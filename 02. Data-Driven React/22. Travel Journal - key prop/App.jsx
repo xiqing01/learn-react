db496cfda24d4b7d3f635521a915b68ea94e5152
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-20 23:26:24 +0800

    Update App.jsx

--- 02. Data-Driven React/22. Travel Journal - key prop/App.jsx
+++ 02. Data-Driven React/22. Travel Journal - key prop/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
     const entryElements = data.map((entry) => {
         return (
             <Entry
+                key={entry.id}
                 img={entry.img}
                 title={entry.title}
                 country={entry.country}
