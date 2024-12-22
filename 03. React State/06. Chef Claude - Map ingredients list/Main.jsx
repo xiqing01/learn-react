adbcdcf915f465f5aee618cf89e43d5ee6987b27
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-22 21:47:40 +0800

    Update Main.jsx

--- 03. React State/06. Chef Claude - Map ingredients list/Main.jsx
+++ 03. React State/06. Chef Claude - Map ingredients list/Main.jsx
@@ -1,26 +1,28 @@
 export default function Main() {
     const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-    
-    /**
-     * Review Challenge:
-     * Map over the list of ingredients and render them as list items
-     * 
-     * Note: We're doing things a weird way here. Don't worry,
-     * we're building up to learning the right way 🙂
-     */
-    
+    const ingredientsListItems = ingredients.map(ingredient => <li key={ingredient}>{ingredient}<li/>)
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredient)
+    }
+        
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form" >
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
             <ul>
-                {/* Render ingredientsList here */}
+                {ingredientsListItems}
             </ul>
         </main>
     )
