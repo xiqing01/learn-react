d3dd9769fc8a8c5c072018169d71e6d7dd071905
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-22 17:26:12 +0800

    Update App.jsx

--- 03. React State/02. Chef Claude - Header/App.jsx
+++ 03. React State/02. Chef Claude - Header/App.jsx
@@ -1,7 +1,4 @@
-/**
- * Challenge: Build the Header component in a separate file
- * and render it here in the App component
- */
+import Header from "./Header"
 
 export default function App() {
   return (
