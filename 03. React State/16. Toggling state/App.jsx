719e4f6bb01efb43bae37757fedd5d18354950d7
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-23 00:53:29 +0800

    Update App.jsx

--- 03. React State/16. Toggling state/App.jsx
+++ 03. React State/16. Toggling state/App.jsx
@@ -1,16 +1,18 @@
-export default function App() {
-    /**
-     * Challenge: 
-     * - Initialize state for `isGoingOut` as a boolean
-     * - Make it so clicking the button flips that
-     *   boolean value (true -> false, false -> true)
-     * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
-     */
+import React from "react"
 
+export default function App() {
+    const [isGoingOut, setIsGoingOut] = React.useState(true)
+    function changeMind() {
+        setIsGoingOut(prev => !prev)
+    }
     return (
         <main>
             <h1 className="title">Do I feel like going out tonight?</h1>
-            <button className="value">Yes</button>
+            <button 
+                onClick={changeMind} 
+                className="value"
+                aria-label={`Current answer is ${isGoingOut ? "Yes" : "No"}. Click to change it.`}
+            >{isGoingOut ? "Yes" : "No"}</button>
         </main>
     )
 }
