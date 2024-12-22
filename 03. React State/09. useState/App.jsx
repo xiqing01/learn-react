f02883c60fbfd0844ef40c3b8b4dd429bff410f2
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-22 22:08:23 +0800

    Update App.jsx

--- 03. React State/09. useState/App.jsx
+++ 03. React State/09. useState/App.jsx
@@ -1,19 +1,14 @@
 import React from "react"
 
 export default function App() {
-    
-    /**
-     * Challenge: Replace our hard-coded "Yes" on the page with 
-     * some state initiated with React.useState()
-     */
-    
+
     const result = React.useState("Hello")
     console.log(result)
     
     return (
         <main>
             <h1 className="title">Is state important to know?</h1>
-            <button className="value">Yes</button>
+            <button className="value">{result[0]}</button>
         </main>
     )
 }
