8a0ae894cde6bbc4bb1722c9574cb77e393330fd
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-22 22:18:27 +0800

    Update App.jsx

--- 03. React State/11. Changing state/App.jsx
+++ 03. React State/11. Changing state/App.jsx
@@ -2,18 +2,15 @@ import React from "react"
 
 export default function App() {
     let [isImportant, setIsImportant] = React.useState("Yes")
-    /**
-     * Challenge: 
-     * 1. Create a function called `handleClick` that runs
-     *    setIsImportant("Definitely")
-     * 2. Add a click event listener to the button
-     *    that runs `handleClick` when the button is clicked.
-     */
+
+    function handleClick() {
+        setIsImportant("Heck yes")
+    }
     
     return (
         <main>
             <h1 className="title">Is state important to know?</h1>
-            <button className="value">{isImportant}</button>
+            <button onClick={handleClick} className="value">{isImportant}</button>
         </main>
     )
 }
