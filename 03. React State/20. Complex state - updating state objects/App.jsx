bfc5475fd16808c7a650aa6d15dbe9ead046c317
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-26 17:57:26 +0800

    Update App.jsx

--- 03. React State/20. Complex state - updating state objects/App.jsx
+++ 03. React State/20. Complex state - updating state objects/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
     function toggleFavorite() {
         setContact(prevContact => {
             return {
+                ...prevContact,
                 isFavorite: !prevContact.isFavorite
             }
         })
