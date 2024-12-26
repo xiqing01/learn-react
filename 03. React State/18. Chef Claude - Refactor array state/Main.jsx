3ecd89e596ae5e23277d62555e5ff45a6d99b4d5
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-26 17:12:05 +0800

    Update Main.jsx

--- 03. React State/18. Chef Claude - Refactor array state/Main.jsx
+++ 03. React State/18. Chef Claude - Refactor array state/Main.jsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 export default function Main() {
 
     /**
@@ -6,7 +8,7 @@ export default function Main() {
      * ingredient to our list!
      */
 
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = React.useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -21,6 +23,7 @@ export default function Main() {
         event.preventDefault()
         const formData = new FormData(event.currentTarget)
         const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
