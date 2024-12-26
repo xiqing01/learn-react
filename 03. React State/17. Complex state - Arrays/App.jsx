8305685889434817acb4a9347933ef31f36eb87e
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-26 17:04:16 +0800

    Update App.jsx

--- 03. React State/17. Complex state - Arrays/App.jsx
+++ 03. React State/17. Complex state - Arrays/App.jsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 export default function App() {
   /**
      * Challenge: Convert the code below to use an array
@@ -6,13 +8,18 @@ export default function App() {
      * 
      * Don't worry about fixing `addFavoriteThing` quite yet.
      */
-  const myFavoriteThings = []
+  const [myFavoriteThings, setMyFavoriteThings] = React.useState([])
   const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁", 
   "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
   const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
 
   function addFavoriteThing() {
-    // We'll work on this next, nothing to do here yet.
+    setMyFavoriteThings(
+      prevFavThings => [
+        ...prevFavThings,
+        allFavoriteThings[prevFavThings.length]
+      ]
+    )
   }
   
   return (
