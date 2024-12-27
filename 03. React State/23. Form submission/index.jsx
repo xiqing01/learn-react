bfedff2599aaeea181bde34013a23849d1c42d66
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-27 17:33:46 +0800

    Update index.jsx

--- 03. React State/23. Form submission/index.jsx
+++ 03. React State/23. Form submission/index.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formEl = event.currentTarget
+    const formData = new FormData(formEl)
+    const email = formData.get("email")
+    formEl.reset( )
+  }
+  
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
+      <form onSubmit={handleSubmit} method="post">
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
         <br />
