ce7202cfefa54ab029aa3016c02653596fb37c59
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-27 17:58:59 +0800

    Update index.jsx

--- 03. React State/27. Forms - radio/index.jsx
+++ 03. React State/27. Forms - radio/index.jsx
@@ -12,6 +12,7 @@ function App() {
   function signUp(formData) {
     const email = formData.get("email")
     const password = formData.get("password")
+    const employmentStatus = formData.get("employmentStatus")
     console.log(password)
   }
 
@@ -32,20 +33,20 @@ function App() {
         <fieldset>
           <legend>Employment Status:</legend>
           <label>
-            <input type="radio" name="employmentStatus" />
+            <input type="radio" name="employmentStatus" value="Unemployed" />
             Unemployed
         </label>
           <label>
-            <input type="radio" name="employmentStatus" />
+            <input type="radio" name="employmentStatus" value="Part-time" />
             Part-time
         </label>
           <label>
-            <input type="radio" name="employmentStatus" />
+            <input type="radio" name="employmentStatus" value="Full-time" />
             Full-time
         </label>
         </fieldset>
 
-
+        
 
         <button>Submit</button>
 
