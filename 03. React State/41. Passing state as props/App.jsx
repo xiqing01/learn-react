5f27a388be21aef383e95e7110b3c561d5dba7a9
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-31 19:00:45 +0800

    Update App.jsx

--- 03. React State/41. Passing state as props/App.jsx
+++ 03. React State/41. Passing state as props/App.jsx
@@ -12,20 +12,6 @@ export default function App() {
         setCount(prevCount => prevCount - 1)
     }
     
-    /**
-     * Challenge:
-     * - Create a new component called `Count`
-     *    - It should receive a prop called `number`, whose value
-     *      is the current value of our count
-     *    - Have the component render the h2.count element below
-     *      and display the incoming prop `number`
-     * - Replace the h2.count below with an instance of
-     *   the new Count component, passing the correct value
-     *   to its `number` prop.
-     * - After doing this, everything should be working the
-     *   same as before.
-     */
-
     return (
         <main className="container">
             <div className="counter">
