21164dda5657f53b1a923ffe87f92edc95a809bc
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-31 19:08:30 +0800

    Update Star.jsx

--- 03. React State/42. Setting state from child components/Star.jsx
+++ 03. React State/42. Setting state from child components/Star.jsx
@@ -1,3 +1,18 @@
+import starFilled from "./images/star-filled.png"
+import starEmpty from "./images/star-empty.png"
 
-
-export default function Star() {}
+export default function Star(props) {
+  return (
+    <button
+                        aria-pressed={props.isFilled}
+                        aria-label={props.isFilled ? "Remove from favorites" : "Add to favorites"}
+                        className="favorite-button"
+                    >
+                        <img
+                            src={starIcon}
+                            alt={props.isFilled ? "filled star icon" : "empty star icon"}
+                            className="favorite"
+                        />
+                    </button>
+  )
+}
