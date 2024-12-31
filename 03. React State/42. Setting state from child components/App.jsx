f149d266c2556aca2479518fa21ee2a173089e43
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-31 19:04:24 +0800

    Update App.jsx

--- 03. React State/42. Setting state from child components/App.jsx
+++ 03. React State/42. Setting state from child components/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import avatar from "./images/user.png"
 import starFilled from "./images/star-filled.png"
 import starEmpty from "./images/star-empty.png"
+import Star from "./Star"
 
 export default function App() {
     const [contact, setContact] = React.useState({
@@ -42,17 +43,7 @@ export default function App() {
                     alt="User profile picture of John Doe"
                 />
                 <div className="info">
-                    <button
-                        aria-pressed={contact.isFavorite}
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-                            className="favorite"
-                        />
-                    </button>
+                    <Star />
                     <h2 className="name">
                         {contact.firstName} {contact.lastName}
                     </h2>
