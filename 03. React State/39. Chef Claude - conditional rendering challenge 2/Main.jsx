59dc456f6c970d84415c54e5794231db37634ed0
xiqing <128521705+xiqing01@users.noreply.github.com>
2024-12-31 15:53:52 +0800

    Update Main.jsx

--- 03. React State/39. Chef Claude - conditional rendering challenge 2/Main.jsx
+++ 03. React State/39. Chef Claude - conditional rendering challenge 2/Main.jsx
@@ -34,13 +34,13 @@ export default function Main() {
             {ingredients.length > 0 && <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                <div className="get-recipe-container">
+                {ingredients.length > 3 && <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div>}
             </section>}
         </main>
     )
