27508ee56b5185d9435a0fbc08b69bf0770a37b3
xiqing <128521705+xiqing01@users.noreply.github.com>
2025-01-02 15:19:39 +0800

    Update Pad.jsx

--- 03. React State/48. Sound pads challenge - part 4.1 - local state/Pad.jsx
+++ 03. React State/48. Sound pads challenge - part 4.1 - local state/Pad.jsx
@@ -1,19 +1,16 @@
-export default function Pad(props) {
-/**
- * Challenge: Create state controlling whether
- * this pad is "on" or "off". Use the incoming
- * `props.on` to determine the initial state.
- * 
- * Create an event listener so when the pad is clicked,
- * it toggles from "on" to "off".
- * 
- * Goal: clicking each pad should toggle it on and off.
- */
+import React from "react"
 
+export default function Pad(props) {
+    const [on, setOn] = React.useState(props.on)
+    function toggle() {
+        setOn(prevOn => !prevOn)
+    }
+    
     return (
         <button 
             style={{backgroundColor: props.color}}
             className={props.on ? "on" : undefined}
+            onClick={toggle}
         ></button>
     )
 }
