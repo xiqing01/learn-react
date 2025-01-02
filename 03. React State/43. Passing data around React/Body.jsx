15806f475e5e19ca6e05a6228b4252c5c290221e
xiqing <128521705+xiqing01@users.noreply.github.com>
2025-01-02 14:32:46 +0800

    Update Body.jsx

--- 03. React State/43. Passing data around React/Body.jsx
+++ 03. React State/43. Passing data around React/Body.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 
-export default function Body() {
+export default function Body(props) {
     return (
         <section>
-            <h1>Welcome back, ___ !</h1>
+            <h1>Welcome back, {props.userName}!</h1>
         </section>
     )
 }
