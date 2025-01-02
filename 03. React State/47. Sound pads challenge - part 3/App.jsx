bc170868cdbbc6f619e3a1005c4201a5c5966311
xiqing <128521705+xiqing01@users.noreply.github.com>
2025-01-02 15:07:48 +0800

    Update App.jsx

--- 03. React State/47. Sound pads challenge - part 3/App.jsx
+++ 03. React State/47. Sound pads challenge - part 3/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
     const [pads, setPads] = React.useState(padsData)
 
     const buttonElements = pads.map(pad => (
-        <Pad key={pad.id} color={pad.color} />
+        <Pad key={pad.id} color={pad.color} on={pad.on} />
     ))
     
     return (
