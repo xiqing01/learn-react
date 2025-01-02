5ae962a05953b04e8bfd27c1519b5033111ada44
xiqing <128521705+xiqing01@users.noreply.github.com>
2025-01-02 16:14:49 +0800

    Update IngredientsList.jsx

--- 03. React State/51. Chef Claude challenge - refactor to separate components/components/IngredientsList.jsx
+++ 03. React State/51. Chef Claude challenge - refactor to separate components/components/IngredientsList.jsx
@@ -1,3 +1,13 @@
-export default function IngredientsList() {
-  return <h1>IngredientsList</h1>
+export default function IngredientsList(props) {
+  return <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{props.ingredientsListItems}</ul>
+                {props.ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+                </div>}
+            </section>
 }
