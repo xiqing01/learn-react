c867b5a97dff5e1949131f0bd14e65693fdb9eae
xiqing <128521705+xiqing01@users.noreply.github.com>
2025-01-03 18:22:42 +0800

    Update Main.jsx

--- 03. React State/55. Challenge - Get recipe from the AI chef/Main.jsx
+++ 03. React State/55. Challenge - Get recipe from the AI chef/Main.jsx
@@ -55,7 +55,7 @@ export default function Main() {
                 />
             }
 
-            {recipe && <ClaudeRecipe />}
+            {recipe && <ClaudeRecipe recipe={recipe} />}
         </main>
     )
 }
