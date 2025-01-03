0d4ff40b303ffa43eb0583e6a906789624040bfe
xiqing <128521705+xiqing01@users.noreply.github.com>
2025-01-03 18:36:37 +0800

    Update ClaudeRecipe.jsx

--- 03. React State/56. Format recipe response/components/ClaudeRecipe.jsx
+++ 03. React State/56. Format recipe response/components/ClaudeRecipe.jsx
@@ -1,19 +1,10 @@
-/**
- * Challenge: See if you can figure out how to use the
- * react-markdown package to render `props.recipe` as React
- * elements rather than the plain markdown text.
- * 
- * Use the link in the slide to navigate directly to the
- * package's instructions on how to use it.
- * 
- * Use the `suggested-recipe-container` class on the <section>
- * below to get some free styling 🙂
- */
+import ReactMarkdown from "react-markdown"
 
 export default function ClaudeRecipe(props) {
     return (
-        <section>
-            {props.recipe}
+        <section className="suggested-recipe-container" aria-live="polite">
+            <h2>Chef Claude Recommends:</h2>
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>   
         </section>
     )
 }
